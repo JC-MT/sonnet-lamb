14263ddfbd2074a6f4dc08b059ee5db91a36e663
Jan Matias <jan@thebeyondgroup.la>
2024-05-17 18:44:44 -0400

    feat | add custom color to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+const customColors = {
+  birch: '#efefef'
+};
+
 export default {
   content: ['./src/**/*.{html,js}', './index.html'],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Brandon Thin']
-      }
+      },
+      colors: customColors
     }
   },
   plugins: []
