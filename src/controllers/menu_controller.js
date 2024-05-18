ad1229cb1cab44e8937fd8df0d6f08873ba2f8f8
Jan Matias <jan@thebeyondgroup.la>
2024-05-18 12:23:18 -0400

    feat | add html gallery route

--- src/controllers/menu_controller.js
+++ src/controllers/menu_controller.js
@@ -2,7 +2,8 @@ import { Controller } from '@hotwired/stimulus';
 const routes = {
   404: './src/pages/404.html',
   '/': './src/pages/home.html',
-  '/About': './src/pages/about.html'
+  '/About': './src/pages/about.html',
+  '/Gallery': './src/pages/gallery.html'
 };
 
 export default class extends Controller {
