9985e14bc223e4f7211c86747e731b9192ea64ab
Jan Matias <jan@thebeyondgroup.la>
2025-04-05 09:24:16 -0400

    chore: add tailwind plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://www.sonnetlamb.com",
   base: "/",
-  adapter: netlify()
+  adapter: netlify(),
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
